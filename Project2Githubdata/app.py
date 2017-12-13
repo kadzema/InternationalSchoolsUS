a757b059ab5c818c7288c69a9dd6479b21bcbeb8
Nagalakshmi <nagalakshmi.pulivarthi@gmail.com>
2017-12-12 19:49:15 -0500

    Added map data

---
+++ Project2Githubdata/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, render_template
+import json
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, func, desc, extract, select, Table
+from collections import defaultdict
+# from dateutil.relativedelta import relativedelta
+        
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    return "xbcbcxhcbx"        
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
